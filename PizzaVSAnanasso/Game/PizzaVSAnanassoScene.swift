6cf7b324e24e8005892e49311413f97fd40b126b
alesdematteo01 <62564507+alesdematteo01@users.noreply.github.com>
2022-03-23 16:16:53 +0100

    jkgk

--- PizzaVSAnanasso/Game/PizzaVSAnanassoScene.swift
+++ PizzaVSAnanasso/Game/PizzaVSAnanassoScene.swift
@@ -17,25 +17,21 @@ class PizzaVSAnanassoScene: SKScene, SKPhysicsContactDelegate {
     let backgroundImage: SKSpriteNode = SKSpriteNode(imageNamed: "background_level")
     
     var ananas_default = SKSpriteNode(imageNamed: "pineapple_sprite0")
-    let ananas_0 = SKTexture(imageNamed: "pineapple_sprite0")
-    let ananas_1 = SKTexture(imageNamed: "pineapple_sprite1")
-    let ananas_2 = SKTexture(imageNamed: "pineapple_sprite2")
-    let ananas_3 = SKTexture(imageNamed: "pineapple_sprite3")
+    var ananasTexture: [SKTexture] = []
     
     let enemy: SKSpriteNode = SKSpriteNode(imageNamed: "enemyChef0")
     let enemy0 = SKTexture(imageNamed: "enemyChef0")
     let enemy1 = SKTexture(imageNamed: "enemyChef1")
     
+    let tagliere0: SKSpriteNode = SKSpriteNode(imageNamed: "tagliere")
+    let tagliere1: SKSpriteNode = SKSpriteNode(imageNamed: "tagliere")
+    let tagliere2: SKSpriteNode = SKSpriteNode(imageNamed: "tagliere")
+    let tagliere3: SKSpriteNode = SKSpriteNode(imageNamed: "tagliere")
+
+    
     override func sceneDidLoad() {
         self.setUpGame()
         self.setUpPhysicsWorld()
-        
-        let ananasAnimation = SKAction.animate(with: [ananas_0, ananas_1, ananas_2, ananas_3], timePerFrame:0.2)
-        ananas_default.run(SKAction.repeatForever(ananasAnimation))
-
-        
-        let enemyAnimation = SKAction.animate(with: [enemy0, enemy1], timePerFrame: 0.2)
-        enemy.run(SKAction.repeatForever(enemyAnimation))
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -52,9 +48,10 @@ extension PizzaVSAnanassoScene {
         
         let playerInitialPosition = CGPoint(x: Positioning.frameX.size.width/2, y: Positioning.frameY.size.height/6)
         self.createPlayer(at: playerInitialPosition)
-        self.generateAnanas()
+        generateAnanas()
         self.createEnemy()
-
+        self.setTaglieri()
+        
     }
     
     private func setUpPhysicsWorld() {
@@ -68,17 +65,52 @@ extension PizzaVSAnanassoScene {
     }
     
     private func setBackground() {
-        self.backgroundImage.position = CGPoint(x: UIScreen.main.bounds.size.width/2, y: UIScreen.main.bounds.size.height/2)
-        self.zPosition = -1
+        backgroundImage.position = CGPoint(x: UIScreen.main.bounds.size.width/2, y: UIScreen.main.bounds.size.height/2)
+        backgroundImage.zPosition = -1
         
         addChild(backgroundImage)
     }
     
+    private func setTaglieri() {
+        self.tagliere0.name = "tagliere0"
+        self.tagliere1.name = "tagliere1"
+        self.tagliere2.name = "tagliere2"
+        self.tagliere3.name = "tagliere3"
+        
+        self.tagliere0.position = CGPoint(x: 0, y: 100)
+        self.tagliere1.position = CGPoint(x: 0, y: 300)
+        self.tagliere2.position = CGPoint(x: 0, y: 500)
+        self.tagliere3.position = CGPoint(x: 0, y: 700)
+        
+        self.tagliere0.zPosition = 1
+        self.tagliere1.zPosition = 1
+        self.tagliere2.zPosition = 1
+        self.tagliere3.zPosition = 1
+        
+        tagliere0.physicsBody = SKPhysicsBody(texture: tagliere0.texture!, size: tagliere0.size)
+        tagliere0.physicsBody?.affectedByGravity = false
+        
+        tagliere1.physicsBody = SKPhysicsBody(texture: tagliere1.texture!, size: tagliere1.size)
+        tagliere1.physicsBody?.affectedByGravity = false
+        
+        tagliere2.physicsBody = SKPhysicsBody(texture: tagliere2.texture!, size: tagliere2.size)
+        tagliere2.physicsBody?.affectedByGravity = false
+        
+        tagliere3.physicsBody = SKPhysicsBody(texture: tagliere3.texture!, size: tagliere3.size)
+        tagliere3.physicsBody?.affectedByGravity = false
+
+        addChild(tagliere0)
+        addChild(tagliere1)
+        addChild(tagliere2)
+        addChild(tagliere3)
+
+    }
+    
     private func createPlayer(at position: CGPoint) {
-        self.player.name = "player"
-        self.player.size = CGSize(width: 64, height: 64)
-        self.player.position = position
-        self.player.zPosition = 1
+        player.name = "player"
+        player.size = CGSize(width: 64, height: 64)
+        player.position = position
+        player.zPosition = 1
         
         player.physicsBody = SKPhysicsBody(circleOfRadius: 25.0)
         player.physicsBody?.affectedByGravity = false
@@ -86,27 +118,33 @@ extension PizzaVSAnanassoScene {
         addChild(self.player)
     }
     
-    private func generateAnanas(){
-        ananas_default = SKSpriteNode(imageNamed: "pineapple_sprite0")
+    func generateAnanas(){
         ananas_default.name = "ananas"
         ananas_default.size = CGSize(width: 64, height: 64)
         ananas_default.position = CGPoint(x: Positioning.frameX.midX, y: Positioning.frameY.midY)
         ananas_default.zPosition = 1
-        ananas_default.position = CGPoint(x: Positioning.frameX.midX, y: Positioning.frameY.midY)
         
+        for i in 0...3{
+            ananasTexture.append(SKTexture(imageNamed: "pineapple_sprite\(i)"))
+        }
         
+        let animation = SKAction.animate(with: ananasTexture, timePerFrame:0.2)
         
         addChild(ananas_default)
+        ananas_default.run(SKAction.repeatForever(animation))
+
     }
     
     private func createEnemy(){
-        self.enemy.name = "enemy"
-        self.enemy.size = CGSize(width: 64, height: 64)
-        self.enemy.position = CGPoint(x: Positioning.frameX.width/3, y: Positioning.frameY.height/3)
-        self.zPosition = 1
+        enemy.name = "enemy"
+        enemy.size = CGSize(width: 64, height: 64)
+        enemy.position = CGPoint(x: Positioning.frameX.width/3, y: Positioning.frameY.height/3)
+        enemy.zPosition = 1
         
+        let animation = SKAction.animate(with: [enemy0, enemy1], timePerFrame: 0.2)
         
         addChild(enemy)
+        enemy.run(SKAction.repeatForever(animation))
     }
 }
 
